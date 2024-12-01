6c55e2ef0d040a60c3cae596a96e6a4a83dbdae9
Michael Whitely <michael.whitely@hotmail.com>
2024-12-02 07:41:26 +1100

    preserve search term

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -13,30 +13,33 @@ export default function VideoCard({ video, onClick }: VideoCardProps) {
     const [showPlaylistManager, setShowPlaylistManager] = useState(false);
 
     return (
-        <div className="bg-white rounded-lg shadow overflow-hidden">
-            <div className="flex gap-3 p-2">
+        <div className="bg-white rounded-lg border border-border overflow-hidden">
+            <div className="flex flew-row gap-3 p-2 h-32">
                 <img
                     src={video.snippet.thumbnails.medium.url}
                     alt={video.snippet.title}
-                    className="w-40 h-24 object-cover rounded cursor-pointer"
+                    className="h-full object-cover rounded cursor-pointer"
                     onClick={onClick}
                 />
-                <div className="flex-1 flex flex-col gap-2">
-                    <h3
-                        className="font-semibold line-clamp-2 text-sm cursor-pointer"
-                        onClick={onClick}
-                    >
-                        {video.snippet.title}
-                    </h3>
-                    <p className="text-xs text-gray-600 mt-1">
-                        {video.snippet.channelTitle}
-                    </p>
-                    <Button
-                        onClick={(e) => {
-                            e.stopPropagation();
-                            setShowPlaylistManager(true);
-                        }}
-                        color={'blue'}
+                <div className="flex-1 flex flex-col gap-2 justify-evenly">
+                    <div>
+                        <h3
+                            className="font-semibold line-clamp-2 text-sm cursor-pointer"
+                            onClick={onClick}
+                        >
+                            {video.snippet.title}
+                        </h3>
+                        <p className="text-xs text-gray-600 mt-1">
+                            {video.snippet.channelTitle}
+                        </p>
+                    </div>
+
+                    <Button className={'flex '}
+                            onClick={(e) => {
+                                e.stopPropagation();
+                                setShowPlaylistManager(true);
+                            }}
+                            color={'blue'}
                     >
                         Add to Playlist
                     </Button>
